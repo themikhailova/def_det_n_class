6e37f05d0ffa0081042bc6adc3f8e3595d24b46b
d-i-s-t-r-a-i-n-t <p13275754@gmail.com>
2024-11-24 21:26:27 +0300

    Update backremoveCV.py

--- preprocess/backremoveCV.py
+++ preprocess/backremoveCV.py
@@ -11,15 +11,17 @@ hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 hsv_image = cv2.GaussianBlur(hsv_image, (11,11), 0)
 
 # Установка диапазона цвета для выделения фона (синий в данном случае)
-lower_blue = np.array([100, 50, 50])  # Нижний порог (темно-синий)
+lower_blue = np.array([100, 50, 50])  # Нижний порог (темно-синий) # изменение s с 50 на 120 оставит ненасыщ. блики на детали, а v с 50 на 0 уберет тени
 upper_blue = np.array([140, 255, 255])  # Верхний порог (светло-синий)
 
 background_mask = cv2.inRange(hsv_image, lower_blue, upper_blue)
 foreground_mask = cv2.bitwise_not(background_mask)
+# kernel = np.ones((15, 15), np.uint8)  # можно использовать, чтобы заполнить небольшие проплешины
+# foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_CLOSE, kernel)
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 filtered_gray = cv2.bitwise_and(gray_image, gray_image, mask=foreground_mask)
-_, binary = cv2.threshold(filtered_gray, 90, 255, cv2.THRESH_BINARY)
+_, binary = cv2.threshold(filtered_gray, 90, 255, cv2.THRESH_BINARY) # 90 заменить на 0, т.к. все лишнее было отфильтровано ранее
 contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 mask_holes = np.zeros_like(gray_image)
@@ -27,7 +29,7 @@ mask_outer = np.zeros_like(gray_image)
 
 for i, contour in enumerate(contours):
     if hierarchy[0][i][3] == -1:  # Внешний контур
-        cv2.drawContours(mask_outer, [contour], -1, color=255, thickness=cv2.FILLED)
+        cv2.drawContours(mask_outer, [contour], -1, color=255, thickness=cv2.FILLED) # тогда здесь получится более цельный внешний контур
         # stack = [i]
         # while stack:
         #     current_idx = stack.pop()
@@ -46,9 +48,16 @@ finish = time.time()
 res_msec = (finish - start) * 1000
 print('Время работы в миллисекундах: ', res_msec)
 
+# Отображение результатов
 plt.figure(figsize=(10, 10))
 plt.imshow(foreground_mask, cmap='gray')
-plt.title('Outer Contour')
+plt.title('Foreground Mask')
+plt.axis('off')
+plt.show()
+
+plt.figure(figsize=(10, 10))
+plt.imshow(mask_outer, cmap='gray')
+plt.title('Outer Contour Mask')
 plt.axis('off')
 plt.show()
 
