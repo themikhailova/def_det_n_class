065ea3aa64c001ca96e4c986245f90cc879fc811
Maria <91223359+themikhailova@users.noreply.github.com>
2024-11-25 10:16:41 +0300

    Update backremoveCV.py

--- preprocess/backremoveCV.py
+++ preprocess/backremoveCV.py
@@ -10,9 +10,29 @@ start = time.time()
 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 hsv_image = cv2.GaussianBlur(hsv_image, (11,11), 0)
 
-# Установка диапазона цвета для выделения фона (синий в данном случае)
+# пороги синего работают нормально везде, кроме 6го фото (глубокая деталь с тенью внутрь)
 lower_blue = np.array([100, 50, 50])  # Нижний порог (темно-синий) # изменение s с 50 на 120 оставит ненасыщ. блики на детали, а v с 50 на 0 уберет тени
 upper_blue = np.array([140, 255, 255])  # Верхний порог (светло-синий)
+
+# зеленый - менее универсальный. при следующих порогах на некоторых фото (пр. 4) не до конца убирает внутренние отверстия
+# lower_green = np.array([0, 100, 0])  # Нижний порог для зелёного
+# upper_green = np.array([120, 255, 255])  # Верхний порог для зелёного
+
+# красный - худший вар. на глубокой детали убирает больше всего, на выпуклых (1 и 5) берет боковую тень
+# lower_red1 = np.array([0, 70, 70])  # Нижний порог для первого диапазона
+# upper_red1 = np.array([50, 255, 255])  # Верхний порог для первого диапазона
+
+# lower_red2 = np.array([50, 50, 0])  # Нижний порог для второго диапазона
+# upper_red2 = np.array([180, 255, 255])  # Верхний порог для второго диапазона
+
+# hsv_image = cv2.GaussianBlur(hsv_image, (5, 5), 0)
+# # Создание масок для двух диапазонов красного
+# mask_red1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
+# mask_red2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
+
+# # Объединение масок
+# background_mask = cv2.bitwise_or(mask_red1, mask_red2)
+# foreground_mask = cv2.bitwise_not(background_mask)
 
 background_mask = cv2.inRange(hsv_image, lower_blue, upper_blue)
 foreground_mask = cv2.bitwise_not(background_mask)
