4a03ec56b3f83fa3f4b320228ea02a661dc2dff1
Валерия Щепилова <valeriashchepilova@192-168-0-101.local>
2024-12-10 12:59:54 +0300

    Поправила виджеты, изменила визуал ИП

--- design/design.py
+++ design/design.py
@@ -14,116 +14,183 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1440, 900)
+        MainWindow.resize(873, 571)
+
+        # Применяем стиль для MainWindow
+        MainWindow.setStyleSheet("""
+            QMainWindow {
+                background-color: #2E3440;
+                color: #ECEFF4;
+                font-family: 'Arial';
+                font-size: 14px;
+            }
+        """)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
-        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_5.setEnabled(False)
-        self.pushButton_5.setObjectName("pushButton_5")
-        self.gridLayout.addWidget(self.pushButton_5, 3, 3, 1, 1)
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setEnabled(False)
-        self.pushButton_4.setAutoDefault(False)
-        self.pushButton_4.setDefault(False)
-        self.pushButton_4.setFlat(False)
-        self.pushButton_4.setObjectName("pushButton_4")
-        self.gridLayout.addWidget(self.pushButton_4, 4, 3, 1, 1)
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setObjectName("label")
-        self.gridLayout.addWidget(self.label, 0, 4, 1, 1)
-        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_6.setObjectName("pushButton_6")
-        self.gridLayout.addWidget(self.pushButton_6, 2, 3, 1, 1)
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setObjectName("pushButton")
-        self.gridLayout.addWidget(self.pushButton, 4, 0, 1, 1)
-        self.listView = QtWidgets.QListView(self.centralwidget)
-        self.listView.setObjectName("listView")
-        self.gridLayout.addWidget(self.listView, 1, 3, 1, 1)
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.gridLayout.addWidget(self.pushButton_2, 4, 1, 1, 1)
-        self.tableView = QtWidgets.QTableView(self.centralwidget)
-        self.tableView.setObjectName("tableView")
-        self.gridLayout.addWidget(self.tableView, 1, 4, 3, 2)
-        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableWidget.setObjectName("tableWidget")
-        self.tableWidget.setColumnCount(0)
-        self.tableWidget.setRowCount(0)
-        self.gridLayout.addWidget(self.tableWidget, 0, 0, 4, 3)
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setObjectName("label_2")
-        self.gridLayout.addWidget(self.label_2, 0, 3, 1, 1)
-        self.pushButton_8 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_8.setObjectName("pushButton_8")
-        self.gridLayout.addWidget(self.pushButton_8, 4, 5, 1, 1)
-        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_7.setObjectName("pushButton_7")
-        self.gridLayout.addWidget(self.pushButton_7, 4, 4, 1, 1)
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.gridLayout.addWidget(self.pushButton_3, 4, 2, 1, 1)
-        self.pushButton_7.raise_()
-        self.pushButton_5.raise_()
-        self.label_2.raise_()
-        self.pushButton_4.raise_()
-        self.label.raise_()
-        self.pushButton_8.raise_()
-        self.tableView.raise_()
-        self.pushButton_6.raise_()
-        self.listView.raise_()
-        self.tableWidget.raise_()
-        self.pushButton_2.raise_()
-        self.pushButton_3.raise_()
-        self.pushButton.raise_()
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1440, 21))
-        self.menubar.setObjectName("menubar")
-        self.menu = QtWidgets.QMenu(self.menubar)
-        self.menu.setObjectName("menu")
-        self.menu_2 = QtWidgets.QMenu(self.menubar)
-        self.menu_2.setObjectName("menu_2")
-        self.menu_3 = QtWidgets.QMenu(self.menubar)
-        self.menu_3.setObjectName("menu_3")
-        MainWindow.setMenuBar(self.menubar)
-        self.action_2 = QtWidgets.QAction(MainWindow)
-        self.action_2.setObjectName("action_2")
-        self.menu.addSeparator()
-        self.menu_3.addAction(self.action_2)
-        self.menubar.addAction(self.menu.menuAction())
-        self.menubar.addAction(self.menu_2.menuAction())
-        self.menubar.addAction(self.menu_3.menuAction())
 
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        # Секция "Настройки"
+        self.setting = QtWidgets.QWidget(self.centralwidget)
+        self.setting.setObjectName("setting")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.setting)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+
+        self.defect = QtWidgets.QGraphicsView(self.setting)
+        self.defect.setObjectName("defect")
+        self.defect.setStyleSheet("""
+            QGraphicsView {
+                border: 2px solid #88C0D0;
+                background-color: #3B4252;
+            }
+        """)
+        self.verticalLayout_3.addWidget(self.defect)
+
+        self.edit = QtWidgets.QPushButton(self.setting)
+        self.edit.setObjectName("edit")
+        self.edit.setStyleSheet(self.button_style("#88C0D0"))
+        self.verticalLayout_3.addWidget(self.edit)
+
+        self.cancel = QtWidgets.QPushButton(self.setting)
+        self.cancel.setObjectName("cancel")
+        self.cancel.setStyleSheet(self.button_style("#BF616A"))
+        self.verticalLayout_3.addWidget(self.cancel)
+
+        self.save = QtWidgets.QPushButton(self.setting)
+        self.save.setObjectName("save")
+        self.save.setStyleSheet(self.button_style("#A3BE8C"))
+        self.verticalLayout_3.addWidget(self.save)
+
+        self.horizontalLayout.addWidget(self.setting)
+
+        # Секция "Изображения"
+        self.imagine = QtWidgets.QWidget(self.centralwidget)
+        self.imagine.setObjectName("imagine")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.imagine)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+
+        self.imagine_label = QtWidgets.QLabel(self.imagine)
+        self.imagine_label.setObjectName("imagine_label")
+        self.imagine_label.setStyleSheet("""
+            QLabel {
+                color: #D8DEE9;
+                font-weight: bold;
+                font-size: 18px;
+            }
+        """)
+        self.verticalLayout_2.addWidget(self.imagine_label)
+
+        self.directory = QtWidgets.QListView(self.imagine)
+        self.directory.setObjectName("directory")
+        self.directory.setStyleSheet("""
+            QListView {
+                border: 2px solid #81A1C1;
+                background-color: #3B4252;
+                color: #D8DEE9;
+            }
+        """)
+        self.verticalLayout_2.addWidget(self.directory)
+
+        self.set_directory = QtWidgets.QPushButton(self.imagine)
+        self.set_directory.setObjectName("set_directory")
+        self.set_directory.setStyleSheet(self.button_style("#88C0D0"))
+        self.verticalLayout_2.addWidget(self.set_directory)
+
+        self.analysis = QtWidgets.QPushButton(self.imagine)
+        self.analysis.setEnabled(True)
+        self.analysis.setObjectName("analysis")
+        self.analysis.setStyleSheet(self.button_style("#81A1C1"))
+        self.verticalLayout_2.addWidget(self.analysis)
+
+        self.statistics = QtWidgets.QPushButton(self.imagine)
+        self.statistics.setEnabled(True)
+        self.statistics.setObjectName("statistics")
+        self.statistics.setStyleSheet(self.button_style("#5E81AC"))
+        self.verticalLayout_2.addWidget(self.statistics)
+
+        self.horizontalLayout.addWidget(self.imagine)
+
+        # Секция "Шаблоны"
+        self.template_3 = QtWidgets.QWidget(self.centralwidget)
+        self.template_3.setObjectName("template_3")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.template_3)
+        self.verticalLayout.setObjectName("verticalLayout")
+
+        self.template_label = QtWidgets.QLabel(self.template_3)
+        self.template_label.setObjectName("template_label")
+        self.template_label.setStyleSheet("""
+            QLabel {
+                color: #D8DEE9;
+                font-weight: bold;
+                font-size: 18px;
+            }
+        """)
+        self.verticalLayout.addWidget(self.template_label)
+
+        self.template_2 = QtWidgets.QTableView(self.template_3)
+        self.template_2.setObjectName("template_2")
+        self.template_2.setStyleSheet("""
+            QTableView {
+                border: 2px solid #88C0D0;
+                background-color: #3B4252;
+                color: #D8DEE9;
+                gridline-color: #88C0D0;
+            }
+        """)
+        self.verticalLayout.addWidget(self.template_2)
+
+        self.delete_template = QtWidgets.QPushButton(self.template_3)
+        self.delete_template.setObjectName("delete_template")
+        self.delete_template.setStyleSheet(self.button_style("#BF616A"))
+        self.verticalLayout.addWidget(self.delete_template)
+
+        self.insert_template = QtWidgets.QPushButton(self.template_3)
+        self.insert_template.setObjectName("insert_template")
+        self.insert_template.setStyleSheet(self.button_style("#A3BE8C"))
+        self.verticalLayout.addWidget(self.insert_template)
+
+        self.horizontalLayout.addWidget(self.template_3)
+
+        MainWindow.setCentralWidget(self.centralwidget)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton_5.setText(_translate("MainWindow", "Провести анализ"))
-        self.pushButton_4.setText(_translate("MainWindow", "Выгрузить статистику"))
-        self.label.setText(_translate("MainWindow", "Шаблоны"))
-        self.pushButton_6.setText(_translate("MainWindow", "Указать директорию"))
-        self.pushButton.setText(_translate("MainWindow", "Изменить"))
-        self.pushButton_2.setText(_translate("MainWindow", "Сохранить"))
-        self.label_2.setText(_translate("MainWindow", "Изображения"))
-        self.pushButton_8.setText(_translate("MainWindow", "Удалить шаблон"))
-        self.pushButton_7.setText(_translate("MainWindow", "Добавить шаблон"))
-        self.pushButton_3.setText(_translate("MainWindow", "Отменить"))
-        self.menu.setTitle(_translate("MainWindow", "Настройки"))
-        self.menu_2.setTitle(_translate("MainWindow", "Справка"))
-        self.menu_3.setTitle(_translate("MainWindow", "Выход"))
-        self.action_2.setText(_translate("MainWindow", "Выход"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Stylish Interface"))
+        self.edit.setText(_translate("MainWindow", "Изменить"))
+        self.cancel.setText(_translate("MainWindow", "Отменить"))
+        self.save.setText(_translate("MainWindow", "Сохранить"))
+        self.imagine_label.setText(_translate("MainWindow", "Изображения"))
+        self.set_directory.setText(_translate("MainWindow", "Указать директорию"))
+        self.analysis.setText(_translate("MainWindow", "Провести анализ"))
+        self.statistics.setText(_translate("MainWindow", "Выгрузить статистику"))
+        self.template_label.setText(_translate("MainWindow", "Шаблоны"))
+        self.delete_template.setText(_translate("MainWindow", "Удалить шаблон"))
+        self.insert_template.setText(_translate("MainWindow", "Добавить шаблон"))
+
+    def button_style(self, color):
+        """Стиль для кнопок"""
+        return f"""
+            QPushButton {{
+                background-color: {color};
+                color: white;
+                border: 2px solid #4C566A;
+                border-radius: 5px;
+                padding: 10px;
+            }}
+            QPushButton:hover {{
+                background-color: #4C566A;
+                color: {color};
+            }}
+        """
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
--- design/main.py
+++ design/main.py
@@ -1,90 +1,100 @@
-from design import Ui_MainWindow  # Импорт сгенерированного файла design.py
-from PyQt5.QtWidgets import QFileDialog
-from PyQt5 import QtWidgets, QtGui
+import sys
 import os
+import shutil
+from PyQt5 import QtWidgets, QtCore
+from design import Ui_MainWindow  # Импорт интерфейса из файла new.py
+
 
 class MainApp(QtWidgets.QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow() 
-        self.ui.setupUi(self)
+        super(MainApp, self).__init__()
+        self.ui = Ui_MainWindow()  # Инициализируем интерфейс
+        self.ui.setupUi(self)  # Устанавливаем интерфейс в MainWindow
 
-        # Подключение кнопок
-        self.ui.pushButton_2.clicked.connect(self.on_save_clicked)
-        self.ui.pushButton_3.clicked.connect(self.on_cancel_clicked)
-        self.ui.pushButton.clicked.connect(self.on_change_clicked)
-        self.ui.pushButton_6.clicked.connect(self.on_set_dir_clicked)
-        self.ui.pushButton_7.clicked.connect(self.on_add_template_clicked)
-        self.ui.pushButton_8.clicked.connect(self.on_delete_template_clicked)
-        self.selected_directory = None  # Переменная для хранения выбранной директории
+        # Храним текущую директорию и выбранный файл
+        self.current_directory = ''
+        self.selected_file = ''
 
-        # Подключение кнопок
-        self.ui.pushButton_2.clicked.connect(self.on_save_image_clicked)  # Кнопка "Сохранить"
-        self.ui.pushButton_6.clicked.connect(self.on_set_directory_clicked)  # Кнопка "Указать директорию"
+        # Подключение сигналов к кнопкам
+        self.ui.edit.clicked.connect(self.on_edit_clicked)
+        self.ui.cancel.clicked.connect(self.on_cancel_clicked)
+        self.ui.save.clicked.connect(self.on_save_clicked)
+        self.ui.set_directory.clicked.connect(self.on_set_directory_clicked)
+        self.ui.analysis.clicked.connect(self.on_analysis_clicked)
+        self.ui.statistics.clicked.connect(self.on_statistics_clicked)
+        self.ui.insert_template.clicked.connect(self.on_insert_template_clicked)
+        self.ui.delete_template.clicked.connect(self.on_delete_template_clicked)
 
-        # Пример: добавляем изображение для сохранения
-        self.image_to_save = None  # Переменная для изображения
+        # Подключаем событие клика по списку файлов
+        self.ui.directory.clicked.connect(self.on_file_selected)
 
-        # Загружаем изображение для примера
-        self.load_example_image()
-
-    def load_example_image(self):
-        """Загрузка изображения для примера (в реальном проекте замените на нужное)."""
-        self.image_to_save = QtGui.QPixmap(300, 300)  # Создаем пустое изображение
-        self.image_to_save.fill(QtGui.QColor("blue"))  # Заливаем цветом
-        # Устанавливаем изображение в QLabel (допустим, у вас есть QLabel для отображения изображения)
-        self.ui.label_2.setPixmap(self.image_to_save)
-
-    def on_set_directory_clicked(self):
-        """Выбор директории для сохранения."""
-        directory = QFileDialog.getExistingDirectory(self, "Выберите директорию")
-        if directory:
-            self.selected_directory = directory
-            print(f"Выбранная директория: {directory}")
-
-    def on_save_image_clicked(self):
-        """Сохранение изображения в выбранную директорию."""
-        if not self.selected_directory:
-            QtWidgets.QMessageBox.warning(self, "Ошибка", "Сначала укажите директорию для сохранения!")
-            return
-
-        if self.image_to_save is None:
-            QtWidgets.QMessageBox.warning(self, "Ошибка", "Нет изображения для сохранения!")
-            return
-
-        # Определяем путь для сохранения изображения
-        file_path = os.path.join(self.selected_directory, "saved_image.png")
-
-        try:
-            # Сохраняем изображение
-            self.image_to_save.save(file_path, "PNG")
-            QtWidgets.QMessageBox.information(self, "Успех", f"Изображение сохранено по пути: {file_path}")
-        except Exception as e:
-            QtWidgets.QMessageBox.critical(self, "Ошибка", f"Не удалось сохранить изображение: {e}")
-
-    def on_save_clicked(self):
-        print("Кнопка 'Сохранить' нажата")
+    def on_edit_clicked(self):
+        print("Кнопка 'Изменить' нажата")
 
     def on_cancel_clicked(self):
         print("Кнопка 'Отменить' нажата")
 
-    def on_change_clicked(self):
-        print("Кнопка 'Изменить' нажата")
+    def on_save_clicked(self):
+        """Сохранить выбранный файл в указанную директорию"""
+        if not self.selected_file:
+            QtWidgets.QMessageBox.warning(self, "Ошибка", "Сначала выберите файл для сохранения.")
+            return
 
-    def on_set_dir_clicked(self):
-        print("Кнопка 'Указать директорию' нажата")
+        target_directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите директорию для сохранения файла")
+        if not target_directory:
+            return  # Если пользователь закрыл диалог без выбора директории
 
-    def on_add_template_clicked(self):
+        # Получаем имя файла из пути и сохраняем файл
+        file_name = os.path.basename(self.selected_file)
+        target_path = os.path.join(target_directory, file_name)
+
+        try:
+            shutil.copy(self.selected_file, target_path)
+            QtWidgets.QMessageBox.information(self, "Успех",
+                                              f"Файл '{file_name}' успешно сохранён в '{target_directory}'")
+            print(f"Файл '{file_name}' сохранён в '{target_path}'")
+        except Exception as e:
+            QtWidgets.QMessageBox.critical(self, "Ошибка", f"Не удалось сохранить файл: {e}")
+            print(f"Ошибка сохранения файла: {e}")
+
+    def on_set_directory_clicked(self):
+        """Установить директорию для отображения изображений"""
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Указать директорию", "")
+        if directory:
+            self.current_directory = directory
+            print(f"Выбрана директория: {directory}")
+            self.load_files_from_directory(directory)
+
+    def on_file_selected(self, index):
+        """Когда пользователь выбирает файл из списка"""
+        if self.current_directory:
+            file_name = self.ui.directory.model().data(index)
+            file_path = os.path.join(self.current_directory, file_name)
+            self.selected_file = file_path
+            print(f"Выбран файл: {file_path}")
+
+    def load_files_from_directory(self, directory):
+        """Загрузить изображения из указанной директории в ListView"""
+        model = QtCore.QStringListModel()
+        files = [f for f in os.listdir(directory) if f.endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+        model.setStringList(files)
+        self.ui.directory.setModel(model)
+
+    def on_analysis_clicked(self):
+        print("Кнопка 'Провести анализ' нажата")
+
+    def on_statistics_clicked(self):
+        print("Кнопка 'Выгрузить статистику' нажата")
+
+    def on_insert_template_clicked(self):
         print("Кнопка 'Добавить шаблон' нажата")
 
     def on_delete_template_clicked(self):
         print("Кнопка 'Удалить шаблон' нажата")
 
 
-
-if __name__ == "__main__":
-    import sys
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    main_app = MainApp()
-    main_app.show()
+    main_window = MainApp()
+    main_window.show()
     sys.exit(app.exec_())
