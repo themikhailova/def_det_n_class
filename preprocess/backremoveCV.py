96377b6315822cf2b9cbb3b4dbfcc06d6c177f14
Maria <91223359+themikhailova@users.noreply.github.com>
2025-01-21 21:53:45 +0300

    Update backremoveCV.py

--- preprocess/backremoveCV.py
+++ preprocess/backremoveCV.py
@@ -1,60 +1,25 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import time
 
-image = cv2.imread(r"./5.jpg")
-start = time.time()
+# image = cv2.imread(r"./def5.jpg")
 
-hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-hsv_image = cv2.GaussianBlur(hsv_image, (15,15), 0)
+def remover(image):
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv_image = cv2.GaussianBlur(hsv_image, (15,15), 0)
 
-# # # Установка диапазона цвета для выделения фона (красный в данном случае)
-# lower_red1 = np.array([0, 70, 70])  # Нижний порог для первого диапазона
-# upper_red1 = np.array([50, 255, 255])  # Верхний порог для первого диапазона
+    lower_green = np.array([0, 100, 0])  # Нижний порог для зелёного
+    upper_green = np.array([240, 255, 255])  # Верхний порог для зелёного
 
-# lower_red2 = np.array([50, 50, 0])  # Нижний порог для второго диапазона
-# upper_red2 = np.array([180, 255, 255])  # Верхний порог для второго диапазона
+    hsv_image = cv2.GaussianBlur(hsv_image, (5, 5), 0)
+    background_mask = cv2.inRange(hsv_image, lower_green, upper_green)
+    foreground_mask = cv2.bitwise_not(background_mask)
+    kernel = np.ones((15, 15), np.uint8)  # можно использовать, чтобы заполнить небольшие проплешины
+    foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_CLOSE, kernel)
 
-# hsv_image = cv2.GaussianBlur(hsv_image, (5, 5), 0)
-# # Создание масок для двух диапазонов красного
-# mask_red1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
-# mask_red2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
-
-# # Объединение масок
-# background_mask = cv2.bitwise_or(mask_red1, mask_red2)
-# foreground_mask = cv2.bitwise_not(background_mask)
+    result_image = cv2.bitwise_and(image, image, mask=foreground_mask)
+    return result_image
 
 
-lower_green = np.array([0, 140, 0])  # Нижний порог для зелёного
-upper_green = np.array([220, 255, 255])  # Верхний порог для зелёного
-
-# Установка диапазона цвета для выделения фона (синий в данном случае)
-# lower_blue = np.array([100, 50, 0])  # Нижний порог (темно-синий) # изменение s с 50 на 120 оставит ненасыщ. блики на детали, а v с 50 на 0 уберет тени
-# upper_blue = np.array([140, 255, 255])  # Верхний порог (светло-синий)
-hsv_image = cv2.GaussianBlur(hsv_image, (5, 5), 0)
-background_mask = cv2.inRange(hsv_image, lower_green, upper_green)
-foreground_mask = cv2.bitwise_not(background_mask)
-kernel = np.ones((15, 15), np.uint8)  # можно использовать, чтобы заполнить небольшие проплешины
-foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_CLOSE, kernel)
-
-result_image = cv2.bitwise_and(image, image, mask=foreground_mask)
-
-finish = time.time()
-res_msec = (finish - start) * 1000
-print('Время работы в миллисекундах: ', res_msec)
-
-plt.figure(figsize=(10, 10))
-plt.imshow(foreground_mask, cmap='gray')
-plt.title('Foreground Mask')
-plt.axis('off')
-plt.show()
-
-
-plt.figure(figsize=(10, 10))
-plt.imshow(result_image, cmap='gray')
-plt.title('result ')
-plt.axis('off')
-plt.show()
-
-cv2.imwrite('./refnoBack.jpg', result_image)
+# cv2.imwrite('./def55.jpg', result_image)
